f5ca01536917ac2230c77cb92675e5e52115e8ab
Javier Cruz López de Ochoa <javcruz@ucm.es>
2021-04-15 16:19:22 +0200

    INIT: proyecto DarkMaze

---
+++ Src/main.cpp
@@ -0,0 +1,25 @@
+#define SDL_MAIN_HANDLED
+
+#include "Engine.h"
+#include "Exceptions.h"
+#include "Logger.h"
+
+#if (defined _DEBUG) || !(defined _WIN32)
+int main() {
+#else
+#include <Windows.h>
+int WINAPI
+WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#endif
+
+	try {
+		Engine* prueba = Engine::getInstance();
+		prueba->init();
+		prueba->run();
+	}
+	catch (const ExcepcionTAD& e) {
+		Logger::getInstance()->log(e.msg(), Logger::Level::FATAL);
+	}
+
+	return 0;
+}
