d265290f1b64be327d518e029f0ce54f7c64a7e5
Javier Cruz López de Ochoa <javcruz@ucm.es>
2021-04-19 17:39:16 +0200

    Player movement sin hacer por falta de metodos en la camara

--- Src/PlayerMovementComponent.cpp
+++ Src/PlayerMovementComponent.cpp
@@ -4,18 +4,29 @@
 #include "KeyboardInput.h"
 #include "Transform.h"
 #include "RigidBodyComponent.h"
+#include "CameraComponent.h"
+#include "MouseInput.h"
+#include "EngineTime.h"
 
 PlayerMovementComponent::PlayerMovementComponent(GameObject* gameObject): Component(UserComponentId::PlayerMovementComponent, gameObject), 
-	_tr(nullptr), _rb(nullptr),
+	_tr(nullptr), _rb(nullptr), _camera(nullptr),
+	_cameraSpeed(1.0f), _pitchAngle(0.0f),
 	_keyForward(KeyCode::KEYCODE_W), _keyLeft(KeyCode::KEYCODE_A), _keyRight(KeyCode::KEYCODE_D), _keyBackward(KeyCode::KEYCODE_S),
-	_speedForward(5), _speedSideways(2), _speedBackwards(2)
+	_speedForward(5), _speedSideways(2), _speedBackwards(2), _slowCreeping(0.5f)
 {
 	_tr = static_cast<Transform*>(_gameObject->getComponent(ComponentId::Transform));
 	_rb = static_cast<RigidBodyComponent*>(_gameObject->getComponent(ComponentId::Rigidbody));
+	_camera = static_cast<CameraComponent*>(_gameObject->getComponent(ComponentId::Camera));
+
+	//MouseInput::getInstance()->setRelative();
 }
 
 void PlayerMovementComponent::update()
 {
+	float deltaTime = EngineTime::getInstance()->deltaTime();
+	
+	moveCamera(deltaTime);
+
 	KeyBoardInput* keyBoard = KeyBoardInput::getInstance();
 	//Front and back movement
 	if (keyBoard->isKeyDown(_keyForward)) {
@@ -32,4 +43,18 @@ void PlayerMovementComponent::update()
 	else if (keyBoard->isKeyDown(_keyRight)) {
 		//Move player to the right
 	}
+}
+
+void PlayerMovementComponent::moveCamera(float deltaTime)
+{
+	MouseInput* mouse = MouseInput::getInstance();
+
+	float deltaX = 0, deltaY = 0;
+	//mouse->getRelativeMovement();
+
+	if (abs(_pitchAngle + deltaY * deltaTime) < 89.9f) {
+		_camera->pitchDegrees(deltaY * deltaTime);
+	}
+
+	_camera->yawDegrees(deltaX * deltaTime);
 }
--- Src/PlayerMovementComponent.h
+++ Src/PlayerMovementComponent.h
@@ -8,19 +8,27 @@
 class GameObject;
 class RigidBodyComponent;
 class Transform;
+class CameraComponent;
 
 class PlayerMovementComponent : public Component {
 public:
+	PlayerMovementComponent() : PlayerMovementComponent(nullptr) {}
 	PlayerMovementComponent(GameObject* gameObject);
 
 	void update();
 private:
+
+	void moveCamera(float deltaTime);
+
 	Transform* _tr;
 	RigidBodyComponent* _rb;
+	CameraComponent* _camera;
+
+	float _cameraSpeed;
+	float _pitchAngle;
 
 	KeyCode _keyForward, _keyLeft, _keyRight, _keyBackward;
-
-	float _speedForward, _speedSideways, _speedBackwards;
+	float _speedForward, _speedSideways, _speedBackwards, _slowCreeping;
 };
 
 #endif // !_PLAYER_MOVEMENT_COMPONENT_
