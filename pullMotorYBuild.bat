5d26ae35fb2edec0219ecce1435919579249c1be
Javier Cruz López de Ochoa <javcruz@ucm.es>
2021-04-20 15:33:32 +0200

    FIX: pull.bat solo compila motor unitario, no el testing

--- pullMotorYBuild.bat
+++ pullMotorYBuild.bat
@@ -4,6 +4,6 @@ git pull
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
 
-msbuild Motor.sln /p:Configuration=Debug /p:Platform=x64
+msbuild Motor.sln /p:Configuration=Debug /p:Platform=x64 /t:MotorUnitario
 
 exit 0;
