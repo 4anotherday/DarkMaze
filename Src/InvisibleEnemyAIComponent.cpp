0225667f7789511fe345c110851bb1b2768bdec8
FenomenCoyote <javcruz@ucm.es>
2021-04-30 10:49:38 +0200

    FEAT: FindState & GoTowardsState logica hecha

--- Src/InvisibleEnemyAIComponent.cpp
+++ Src/InvisibleEnemyAIComponent.cpp
@@ -3,13 +3,16 @@
 #include "Transform.h"
 #include "Engine.h"
 #include "GameObject.h"
+#include "RigidBodyComponent.h"
+#include <random>
+#include "EngineTime.h"
 
-#define CAST_COMPONENT component = static_cast<InvisibleEnemyAIComponent*>(component)
+#define CAST_COMPONENT InvisibleEnemyAIComponent* comp = static_cast<InvisibleEnemyAIComponent*>(component);
 
 ADD_COMPONENT(InvisibleEnemyAIComponent)
 
 InvisibleEnemyAIComponent::InvisibleEnemyAIComponent() : Component(UserComponentId::InvisibleEnemyAIComponent),
-	_transformPlayer(nullptr), _ai(nullptr), _states(), _transitions()
+	_transformPlayer(nullptr), _ai(nullptr), _states(), _transitions(), _radiusFindPlayer(15), _minRadiusFindPlayer(4), _maxRadiusFindPlayer(20)
 {
 
 }
@@ -27,7 +30,7 @@ InvisibleEnemyAIComponent::~InvisibleEnemyAIComponent()
 
 void InvisibleEnemyAIComponent::awake(luabridge::LuaRef& data)
 {
-	GameObject* player = Engine::getInstance()->findGameObject("player");
+	GameObject* player = Engine::getInstance()->findGameObject("Player");
 
 	_transformPlayer = GETCOMPONENT(Transform, ComponentId::Transform);
 
@@ -43,25 +46,34 @@ void InvisibleEnemyAIComponent::createFSM(luabridge::LuaRef& data)
 {
 	_ai = new FSM(this);
 
-	State* find = createState<FindState>();
+	FindState* find = createState<FindState>(); 
+	find->setRadius(&_radiusFindPlayer);
 	State* towardsPlayer = createState<GoTowardsPlayerState>();
 	State* attackPlayer = createState<AttackPlayerState>();
+	State* towardsSound = createState<TowardsSoundState>();
 
 	Transition* lostSightTransition = createTransition<LostSightTransition>();
 	Transition* gainSightTransition = createTransition<GainSightTransition>();
 	Transition* inRangeTransition = createTransition<InRangeTransition>();
+	Transition* loudSoundTransition = createTransition<LoudSoundTransition>();
+	Transition* noSoundTransition = createTransition<NoSoundTransition>();
 
 	_ai->add(find, gainSightTransition, towardsPlayer);
-	_ai->add(towardsPlayer, lostSightTransition, find);
+	_ai->add(find, loudSoundTransition, towardsSound);
 
+	_ai->add(towardsPlayer, lostSightTransition, find);
 	_ai->add(towardsPlayer, inRangeTransition, attackPlayer);
+
+	_ai->add(towardsSound, noSoundTransition, find);
+	_ai->add(towardsSound, gainSightTransition, towardsPlayer);
+
 	_ai->add(attackPlayer, lostSightTransition, find);
 
 	_ai->setInitialState(find);
 }
 
 template<typename T>
-State* InvisibleEnemyAIComponent::createState()
+T* InvisibleEnemyAIComponent::createState()
 {
 	T* state = new T();
 	_states.push_back(state);
@@ -69,7 +81,7 @@ State* InvisibleEnemyAIComponent::createState()
 }
 
 template<typename T>
-Transition* InvisibleEnemyAIComponent::createTransition()
+T* InvisibleEnemyAIComponent::createTransition()
 {
 	T* transition = new T();
 	_transitions.push_back(transition);
@@ -85,16 +97,44 @@ Transition* InvisibleEnemyAIComponent::createTransition()
 void InvisibleEnemyAIComponent::FindState::execute(Component* component)
 {
 	CAST_COMPONENT;
+
+	comp->updateRadiusFind(0.1 * EngineTime::getInstance()->deltaTime());
+
+	double distance = (comp->getMyTransform()->getPosition() - _targetPos).magnitude();
+	if (distance < 2) {
+		findNewTargetPos(comp->getPlayerTransform()->getPosition());
+	}
+	moveToTargetPos();
+}
+
+void InvisibleEnemyAIComponent::FindState::findNewTargetPos(const Vector3& playerPos)
+{
+	Vector3 dirVector = Vector3(-1 + 2 * ((float)rand()) / RAND_MAX, -1 + 2 * ((float)rand()) / RAND_MAX, -1 + 2 * ((float)rand()) / RAND_MAX).normalize();
+	_targetPos = playerPos + (dirVector * *_radiusFromPlayer);
+}
+
+void InvisibleEnemyAIComponent::FindState::moveToTargetPos()
+{
+	//TODO: Moverse hacia _targetPos
 }
 
 void InvisibleEnemyAIComponent::GoTowardsPlayerState::execute(Component* component)
 {
 	CAST_COMPONENT;
+	const Vector3& targetPos = comp->getPlayerTransform()->getPosition();
+
+
+	//TODO: Moverse hacia targetPos
 }
 
 void InvisibleEnemyAIComponent::AttackPlayerState::execute(Component* component)
 {
 	CAST_COMPONENT;
+
+	double distance = (comp->getMyTransform()->getPosition() - comp->getPlayerTransform()->getPosition()).magnitude();
+	if (distance < 2) {
+		//TODO: Atacar
+	}
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------
@@ -112,11 +152,27 @@ bool InvisibleEnemyAIComponent::LostSightTransition::evaluate(Component* compone
 bool InvisibleEnemyAIComponent::GainSightTransition::evaluate(Component* component)
 {
 	CAST_COMPONENT;
+	comp->setRadius(0.0);
+	return false;
+}
+
+bool InvisibleEnemyAIComponent::LoudSoundTransition::evaluate(Component* component)
+{
+	CAST_COMPONENT;
 	return false;
 }
 
 bool InvisibleEnemyAIComponent::InRangeTransition::evaluate(Component* component)
 {
 	CAST_COMPONENT;
+	return false;
+}
+
+void InvisibleEnemyAIComponent::TowardsSoundState::execute(Component* component)
+{
+}
+
+bool InvisibleEnemyAIComponent::NoSoundTransition::evaluate(Component* component)
+{
 	return false;
 }
--- Src/InvisibleEnemyAIComponent.h
+++ Src/InvisibleEnemyAIComponent.h
@@ -4,8 +4,10 @@
 
 #include "Component.h"
 #include "FSM/FSM.h"
-
+#include "Vector3.h"
+#include <algorithm>
 class Transform;
+
 
 class InvisibleEnemyAIComponent : public Component {
 public:
@@ -17,43 +19,109 @@ public:
 
 	void update() override;
 
+	inline Transform* getMyTransform() const { return _myTransform; }
+	inline Transform* getPlayerTransform() const { return _transformPlayer; }
+
+	inline void updateRadiusFind(double delta) { _radiusFindPlayer = std::min(_maxRadiusFindPlayer, std::max(_minRadiusFindPlayer, (_radiusFindPlayer + delta))); }
+
+	inline void setRadius(double radius) { _radiusFindPlayer = std::min(_maxRadiusFindPlayer, std::max(_minRadiusFindPlayer, radius));}
+
 private:
 
 	void createFSM(luabridge::LuaRef& data);
 
 	template <typename T>
-	State* createState();
+	T* createState();
 
 	template <typename T>
-	Transition* createTransition();
+	T* createTransition();
 
 	Transform* _transformPlayer;
+	Transform* _myTransform;
 
 	FSM* _ai;
 
 	std::list<State*> _states;
 	std::list<Transition*> _transitions;
 
+	double _radiusFindPlayer, _maxRadiusFindPlayer, _minRadiusFindPlayer;
+
 	//States
 	class FindState : public State { 
+	public:	
 		void execute(Component* component); 
-	};
-	class GoTowardsPlayerState : public State { 
-		void execute(Component* component);
-	};
-	class AttackPlayerState : public State { 
-		void execute(Component* component); 
+		inline void setRadius(double* radius) { _radiusFromPlayer = radius; }
+	private:
+		void findNewTargetPos(const Vector3& playerPos);
+		void moveToTargetPos();
+
+		double* _radiusFromPlayer;
+		Vector3 _targetPos;
 	};
 
+	class GoTowardsPlayerState : public State { 
+	public:
+
+		void execute(Component* component);
+	private:
+
+	};
+
+	class AttackPlayerState : public State { 
+	public:
+
+		void execute(Component* component); 
+	private:
+
+	};
+
+	class TowardsSoundState : public State {
+	public:
+
+		void execute(Component* component);
+	private:
+
+	};
+
+
 	//Transitions
-	class LostSightTransition : public Transition { 
+	class LostSightTransition : public Transition {
+	public:
+
 		bool evaluate(Component* component); 
+	private:
 	};
+
 	class GainSightTransition : public Transition { 
+	public:
+
 		bool evaluate(Component* component);
+	private:
+
 	};
+
+	class LoudSoundTransition : public Transition {
+	public:
+
+		bool evaluate(Component* component);
+	private:
+
+	};
+
+	class NoSoundTransition : public Transition {
+	public:
+
+		bool evaluate(Component* component);
+	private:
+
+	};
+
 	class InRangeTransition : public Transition { 
+	public:
+
 		bool evaluate(Component* component); 
+	private:
+
 	};
 };
 
