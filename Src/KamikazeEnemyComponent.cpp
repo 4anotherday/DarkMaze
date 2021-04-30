e499bda2351c99c0d8dfa38db0bb6a34b6b01e0f
nicoFdez <nicolafe@ucm.es>
2021-04-30 10:42:53 +0200

    Init Enemigo kamikaze

---
+++ Src/KamikazeEnemyComponent.cpp
@@ -0,0 +1,22 @@
+#include "KamikazeEnemyComponent.h"
+//#include "IncludeLuah.h"
+
+KamikazeEnemyComponent::KamikazeEnemyComponent()
+{
+}
+
+KamikazeEnemyComponent::~KamikazeEnemyComponent()
+{
+}
+
+void KamikazeEnemyComponent::awake(luabridge::LuaRef& data)
+{
+}
+
+void KamikazeEnemyComponent::start()
+{
+}
+
+void KamikazeEnemyComponent::update()
+{
+}
