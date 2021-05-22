a2f5fa270a530faf5e6e35540cf37414bb57e6fa
nicoFdez <nicolafe@ucm.es>
2021-05-22 12:37:49 +0200

    FIX: Trampa 1 colocada del todo

--- bin/Assets/Levels/nivel1.lua
+++ bin/Assets/Levels/nivel1.lua
@@ -85,7 +85,7 @@ go_6[4] = { Component = "HideoutComponent" }
 -- Trampa
 go_7 = {}
 go_7[0] = { Name = "Trampa1", HowManyCmps = 5, Persist = false}
-go_7[1] = { Component = "Transform", Coord = {X = -16.8, Y = 0.1, Z = -11.35}, Rotation = {X = -90, Y = 0, Z = 0}, Scale = {X = 2, Y = 1.3, Z = 4}}
+go_7[1] = { Component = "Transform", Coord = {X = -16.8, Y = 0.1, Z = -11.7}, Rotation = {X = -90, Y = 0, Z = 0}, Scale = {X = 1.6, Y = 1.8, Z = 3.6}}
 go_7[2] = {	Component = "RenderObject", MeshName="spikes.mesh", Material="pincho", Visible=true, Shadows=true, RenderingDistance = 50}
 go_7[3] = { Component = "BoxCollider", Type="Box",Width=1,Height=1,Depth=50,IsTrigger=true}
 go_7[4] = { Component = "TrapComponent",EnabledMaterial="Practica1/Red"}
@@ -94,7 +94,7 @@ go_7[5] = { Component = "AudioSource", Route = {"Assets/Audio/MetalHit.wav" }, S
 -- Desactivador
 go_8 = {}
 go_8[0] = { Name = "Desactivador1", HowManyCmps = 5, Persist = false}
-go_8[1] = { Component = "Transform", Coord = {X = -16.75, Y = 0.2, Z = -12.2}, Rotation = {X = 0, Y = 0, Z = 0}, Scale = {X = .3, Y = .4, Z = .3}}
+go_8[1] = { Component = "Transform", Coord = {X = -16.8, Y = 0.2, Z = -12.2}, Rotation = {X = 0, Y = 0, Z = 0}, Scale = {X = .3, Y = .4, Z = .3}}
 go_8[2] = {	Component = "RenderObject", MeshName="cube.mesh", Material="Practica1/Metal", Visible=true, Shadows=true, RenderingDistance = 50}
 go_8[3] = { Component = "SphereCollider",Radius=2,IsTrigger=true}
 go_8[4] = { Component = "ObjectDeactivatorComponent",ObjName="Trampa1"}
