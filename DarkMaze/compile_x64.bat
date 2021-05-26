de0922c6621fea9459e2faf980104b7c5f426951
nicoFdez <nicolafe@ucm.es>
2021-05-26 17:39:05 +0200

    Arreglos compilacion automática

--- DarkMaze/compile_x64.bat
+++ DarkMaze/compile_x64.bat
@@ -1,7 +1,8 @@
 rem copia las dlls en caso de que sean nuevas -noexistan- o se hayan modificado. Version inicial 2.01.08
 
 rem SDL
-xcopy ..\Motor\dependencies\SDL2\Buildx64\SDL2.dll ..\bin\ /s /d /y
+xcopy ..\Motor\dependencies\SDL2\Build\Debug\SDL2d.dll ..\bin\ /s /d /y
+xcopy ..\Motor\dependencies\SDL2\Build\Release\SDL2.dll ..\bin\ /s /d /y
 
 rem FMOD
 xcopy ..\Motor\dependencies\Fmod\Buildx64\fmod.dll ..\bin\ /s /d /y
--- pullMotorYBuild.bat
+++ pullMotorYBuild.bat
@@ -2,3 +2,4 @@ cd Motor/
 git checkout main
 git pull
 call Compile.bat
+msbuild DarkMaze.sln /p:Configuration=Release /p:Platform=x64
