b8208ec7ac862c24af940da0e8f4febc2bb52161
nicoFdez <nicolafe@ucm.es>
2021-05-26 13:27:04 +0200

    UPDATE: Update pullMotorYBuild.bat

--- pullMotorYBuild.bat
+++ pullMotorYBuild.bat
@@ -1,9 +1,4 @@
 cd Motor/
 git checkout main
 git pull
-
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
-
-msbuild Motor.sln /p:Configuration=Debug /p:Platform=x64 /t:MotorUnitario
-
-exit 0;
+call Compile.bat
